a6402895994f5173b2cdabb01414d5c590a68ada
Andrey Glumov <gluman@yandex.ru>
2025-01-26 10:03:32 +0300

    fix errors

--- task.js
+++ task.js
@@ -4,12 +4,12 @@ setInterval(() => {
     revealdiv.forEach(function (d) {
         if (d.classList.contains('reveal')) {
             const { top, bottom } = d.getBoundingClientRect()
-            console.log(top, bottom, window.innerHeitgh)
+            console.log(top, bottom, window.innerHeight)
             // console.log()
             if (bottom < 0) {
                 d.classList.remove('reveal_active')
             }
-            else if (top > window.innerHeitgh) {
+            else if (top > window.innerHeight) {
                 d.classList.remove('reveal_active')
             }
             else {
